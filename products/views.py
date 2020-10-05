92801eb4f309f16f17a247397463021ec348b5df
AbnocChinwads <rnemo@hotmail.co.uk>
2020-10-05 11:35:36 +0000

    Added add to bag functionality

--- products/views.py
+++ products/views.py
@@ -1,12 +1,11 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.db.models.functions import Lower
 
 from .models import Product, Category
 
 # Create your views here.
-
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -31,7 +30,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -40,9 +39,9 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -61,7 +60,7 @@ def all_products(request):
 def product_detail(request, product_id):
     """ A view to show individual product details """
 
-    product = get_object_or_404(product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
